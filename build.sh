e4c8d8fbba27ae654543ee28aeb5e6bec21f2def
m0a07xh <manjunathasundi07@gmail.com>
2024-07-21 20:02:25 +0530

    docker changes

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-docker image rm manju0707/sample-springboot-microservice-docker-cloud-config-server:0.0.1-SNAPSHOT -f
+docker image rm manju0707/crowd-projects-app-microservice-docker-cloud-config-server:0.0.1-SNAPSHOT -f
 cd cloud-config-server
 mvn clean package
 
-docker image rm manju0707/sample-springboot-microservice-docker-eureka-naming-server:0.0.1-SNAPSHOT -f
+docker image rm manju0707/crowd-projects-app-microservice-docker-eureka-naming-server:0.0.1-SNAPSHOT -f
 cd ../eureka-naming-server
 mvn clean package
 
-docker image rm manju0707/sample-springboot-microservice-docker-oauth2-authentication-server:0.0.1-SNAPSHOT -f
+docker image rm manju0707/crowd-projects-app-microservice-docker-oauth2-authentication-server:0.0.1-SNAPSHOT -f
 cd ../oauth2-authentication-server
 mvn clean package
 
-docker image rm manju0707/sample-springboot-microservice-docker-user-service:0.0.1-SNAPSHOT -f
+docker image rm manju0707/crowd-projects-app-microservice-docker-user-service:0.0.1-SNAPSHOT -f
 cd ../user-service
 mvn clean package
 
-docker image rm manju0707/sample-springboot-microservice-docker-project-service:0.0.1-SNAPSHOT -f
+docker image rm manju0707/crowd-projects-app-microservice-docker-project-service:0.0.1-SNAPSHOT -f
 cd ../project-service
 mvn clean package
 
-docker image rm manju0707/sample-springboot-microservice-docker-zuul-api-gateway-server:0.0.1-SNAPSHOT -f
+docker image rm manju0707/crowd-projects-app-microservice-docker-zuul-api-gateway-server:0.0.1-SNAPSHOT -f
 cd ../zuul-api-gateway-server
 mvn clean package
 
